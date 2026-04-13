14dffb2d8b7c4372d39ab826afc6213998c4a46e
Manish Vyas <manishvyas393@gmail.com>
2026-04-13 12:43:02 +0530

    add transitions in pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { HomePage } from "./pages/HomePage.js";
 import { ReviewPage } from "./pages/ReviewPage.js";
 
 export default function App(): JSX.Element {
+  const location = useLocation();
+
   return (
-    <Routes>
+    <Routes location={location} key={location.pathname}>
       <Route path="/" element={<HomePage />} />
       <Route path="/review/:id" element={<ReviewPage />} />
     </Routes>
